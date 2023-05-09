c1aa193a0081a7fbe276088faac092f846bdc92b
LuisMi-CAfuera <113511965+LuisMi-CAfuera@users.noreply.github.com>
2023-05-09 11:56:00 +0200

    Camara_Terminada(Solo_Falta_Lago)

---
+++ TrabajoFinalGrado/Assets/Scripts/Extras/ZonaConfinder.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class ZonaConfinder : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera camara;
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.CompareTag("Player"))
+        {
+            camara.gameObject.SetActive(true);
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            camara.gameObject.SetActive(false);
+        }
+    }
+}
